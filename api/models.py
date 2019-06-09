8ce3c79d2edadc60baedfceb63dc8df6962838a6
John Skolfield <jskolfield@dataxu.com>
2019-06-09 15:01:57 -0400

    add protein model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+class Protein(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1024)
+    code = models.CharField(max_length=32)
+    sequence_file_name = models.CharField(max_length=255)
+
 class Search(models.Model):
 
     id = models.AutoField(primary_key=True)
